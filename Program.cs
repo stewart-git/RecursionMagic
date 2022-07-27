9f09c0be9d7af4ae0aca5e2876fe11c199c755a8
stewart-git <Stewart.Godwin@smtafe.wa.edu.au>
2022-07-27 13:28:56 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+// Recursion
+
+namespace RecursionMagic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 6; // int.Parse(args[0]);
+            mystery(x);
+            Console.ReadLine();
+        }
+        static void mystery(int n)
+        {
+            if (n < 1)
+            {
+                Console.WriteLine("Finished");
+            }
+            else
+            {
+                Console.WriteLine(n); // going up
+                mystery(n - 1);
+                Console.WriteLine(n); // going down
+            }
+        }
+    }
+}
